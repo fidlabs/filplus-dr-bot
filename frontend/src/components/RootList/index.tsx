b75438edc90e43b15bdc4e1aae0912b5656d8875
Szymon.Zieba <szymon.zieba@neti-soft.com>
2024-03-19 15:23:30 +0100

    add header to table, change signer addresses to long id, add full table data, handle already signed button, handle loading issue and no issue

--- frontend/src/components/RootList/index.tsx
+++ frontend/src/components/RootList/index.tsx
@@ -1,76 +1,103 @@
-import {useEffect, useState} from 'react';
+import {useContext, useEffect, useState} from 'react';
 import {DataCap} from '../../types/DataCap';
 import {getPendingIssues} from '../../api';
 import useLedgerWallet from '../../hooks/useLedgerWallet';
 import {SubmitRemoveData} from '../../types/SubmitRemoveDataCap';
-import {TableCell, TableRow, TableHead, Button} from '@mui/material';
+import {TableCell, TableRow, TableHead, Button, TableBody} from '@mui/material';
+import {DeviceContext} from '../Context/DeviceContext';
 
 const RootList = () => {
-	const [pendingIssues, setPendingIssues] = useState<
-		DataCap[]
-	>([]);
+	const [pendingIssues, setPendingIssues] = useState<DataCap[]>([]);
 	const {submitRemoveDataCap} = useLedgerWallet();
+	const {currentAccount} = useContext(DeviceContext);
 
 	useEffect(() => {
-		getNotaryList()
+		getNotaryList();
 	}, []);
-	const getNotaryList = async() => {
-		const issues = await getPendingIssues()
-		setPendingIssues(issues)
-	}
+	const getNotaryList = async () => {
+		const issues = await getPendingIssues();
+		setPendingIssues(issues);
+	};
 	const onSignRemoveDataCap = async (submitRemoveData: SubmitRemoveData) => {
 		await submitRemoveDataCap(submitRemoveData);
-		await getNotaryList()
+		await getNotaryList();
 	};
-	if (!pendingIssues || pendingIssues.length < 1) return;
-	return (
-		<TableHead>
-			{pendingIssues.map((pendingIssue) => {
-				const {
-					member,
-					allocation,
-					issue,
-					stale,
-					signature1,
-					notary1,
-					signature2,
-					notary2,
-					txFrom,
-					msigTxId,
-				} = pendingIssue;
-				const submitRemoveData: SubmitRemoveData = {
-					allocation: BigInt(allocation),
-					sig1: signature1 || '',
-					notary1: notary1 || '',
-					notary2: notary2 || '',
-					sig2: signature2 || '',
-					txFrom,
-					msigTxId,
-					clientAddress: member,
-					issue,
-				};
+	if (!pendingIssues) {
+		return <div>Loading...</div>;
+	}
 
-				if (stale !== "true") return;
-				return (
-					<TableRow
-						key={member + allocation}
-						style={{display: 'flex', gap: '50px'}}
-					>
-						<TableCell style={{border: 'none'}}>{member}</TableCell>
-						<TableCell style={{border: 'none'}}>{allocation}</TableCell>
-						<TableCell style={{border: 'none'}}>{issue}</TableCell>
-						<TableCell style={{border: 'none'}}>
-							<Button
-								variant="contained"
-								onClick={() => onSignRemoveDataCap(submitRemoveData)}
-							>
-								Sign
-							</Button>
-						</TableCell>
-					</TableRow>
-				);
-			})}
-		</TableHead>
+	if (pendingIssues.length < 1) {
+		return <div>No issues found...</div>;
+	}
+
+	const cellStyle = {border: 'none'};
+	return (
+		<>
+			<TableHead>
+				<TableRow style={{display: 'flex', gap: '50px'}}>
+					<TableCell style={cellStyle}>Client Name</TableCell>
+					<TableCell style={cellStyle}>Client Address</TableCell>
+					<TableCell style={cellStyle}>Removed Datacap</TableCell>
+					<TableCell style={cellStyle}>Github Link</TableCell>
+					<TableCell style={cellStyle}>Signatures</TableCell>
+				</TableRow>
+			</TableHead>
+			<TableBody>
+				{pendingIssues.map((pendingIssue) => {
+					const {
+						member,
+						allocation,
+						issue,
+						stale,
+						signature1,
+						notary1,
+						signature2,
+						notary2,
+						txFrom,
+						msigTxId,
+						clientName,
+						issueGov,
+					} = pendingIssue;
+					const submitRemoveData: SubmitRemoveData = {
+						allocation: BigInt(allocation),
+						sig1: signature1 || '',
+						notary1: notary1 || '',
+						notary2: notary2 || '',
+						sig2: signature2 || '',
+						txFrom,
+						msigTxId,
+						clientAddress: member,
+						issue,
+					};
+
+					if (stale !== 'true') return;
+					const isAlreadySignByUser = txFrom === currentAccount;
+					return (
+						<TableRow
+							key={member + allocation}
+							style={{display: 'flex', gap: '50px'}}
+						>
+							<TableCell style={cellStyle}>{clientName}</TableCell>
+							<TableCell style={cellStyle}>{member}</TableCell>
+							<TableCell style={cellStyle}>{allocation}</TableCell>
+							<TableCell style={cellStyle}>
+								<a href={issueGov}>Issue Link</a>
+							</TableCell>
+							<TableCell style={cellStyle}>{txFrom ? '1/2' : '0/2'}</TableCell>
+							<TableCell style={cellStyle}>
+								<Button
+									disabled={isAlreadySignByUser}
+									variant="contained"
+									onClick={() => !isAlreadySignByUser && onSignRemoveDataCap(submitRemoveData)}
+								>
+									{isAlreadySignByUser ? 'Already Signed' : 'Sign'}
+								</Button>
+							</TableCell>
+						</TableRow>
+					);
+				})}
+			</TableBody>
+		</>
 	);
 };
 
