534286ca7b1ad7d1db470ce1245d9b8b69df8d8a
Filip Lelek <filip.lelek@neti-soft.com>
2024-03-20 10:49:57 +0100

    Fix part of the bugs

--- frontend/src/types/DataCap.ts
+++ frontend/src/types/DataCap.ts
@@ -1,15 +1,16 @@
 export type DataCap = {
-  allocation: string,
-  date: string,
-  issue: string,
-  stale: string,
-  member: string,
-  notary1?: string,
-  notary2?: string,
-  signature1?: string,
-  signature2?: string,
-  msigTxId?: string,
-  txFrom?: string,
-  clientName: string;
-  issueGov: string;
-}
+	allocation: string;
+	date: string;
+	issue: string;
+	stale: string;
+	member: string;
+	notary1?: string;
+	notary2?: string;
+	signature1?: string;
+	signature2?: string;
+	msigTxId?: string;
+	txFrom?: string;
+	clientName: string;
+	issueGov: string;
+	linkIssueGov: string;
+};
